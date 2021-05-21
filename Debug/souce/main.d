a4631b9a118f86914d0c45d1bd6e0b5e1afb656e
capricorn99 <darlinreadback@gmail.com>
2021-05-22 00:17:13 +0700

    finished_worked

--- Debug/souce/main.d
+++ Debug/souce/main.d
@@ -15,17 +15,19 @@ souce/main.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/s
 souce/main.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/stdlib.h
 souce/main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_types.h
 souce/main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_memmap.h
+souce/main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_gpio.h
+souce/main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/tm4c123gh6pm.h
+souce/main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_timer.h
 souce/main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/sysctl.h
 souce/main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/gpio.h
 souce/main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/systick.h
 souce/main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/debug.h
 souce/main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/pwm.h
 souce/main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/pin_map.h
-souce/main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_gpio.h
 souce/main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/interrupt.h
-souce/main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/tm4c123gh6pm.h
 souce/main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/uart.h
 souce/main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/timer.h
+souce/main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/rom.h
 
 ../souce/main.c: 
 C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/stdint.h: 
@@ -42,14 +44,16 @@ C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/stdbool.h:
 C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/stdlib.h: 
 C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_types.h: 
 C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_gpio.h: 
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/tm4c123gh6pm.h: 
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_timer.h: 
 C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/sysctl.h: 
 C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/gpio.h: 
 C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/systick.h: 
 C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/debug.h: 
 C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/pwm.h: 
 C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/pin_map.h: 
-C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_gpio.h: 
 C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/interrupt.h: 
-C:/ti/TivaWare_C_Series-2.2.0.295/inc/tm4c123gh6pm.h: 
 C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/uart.h: 
 C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/timer.h: 
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/rom.h: 
